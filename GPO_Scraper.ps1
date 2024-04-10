57e5a04feaf23b75f1c3e0216183f5475d9aaf2b
Connor Walton <101714236+who-GNU@users.noreply.github.com>
2024-04-10 16:22:37 -0500

    Add files via upload

--- GPO_Scraper.ps1
+++ GPO_Scraper.ps1
@@ -1,32 +1,33 @@
 ##Script for getting all GPO linked to OU and additional info
 ##
 
-$StartOU = #xxx
+$startOU = #OU to start with
+$filePath = #File path to save CSV to 
 
-(Get-GPInheritance -Target $StartOU).GpoLinks | ForEach-Object{
-    $data = [PSCustomObject]@{
-        OU          = $_.Path 
-        DisplayName = (Get-GPO -Name ($_.DisplayName)).DisplayName
-        Description = (Get-GPO -Name ($_.DisplayName)).Description
-        WMIFilter   = (Get-GPO -Name ($_.DisplayName)).WMIFilter
-   
-    }
+$fullList = $null #clears the variable in case you've used it previously (i.e: ran the script this session)
+$fullList += ((Get-GPInheritance -Target $startOU).GpoLinks | Select-Object DisplayName, Target, Description, WMIFilter) #Adds the GPO name + linked OU to the $fullList variable array
 
-Write-Output $data | Export-CSV ##PATH
+Write-Host "Links from starting OU found..."#write to give status update
 
+(Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase $startOU -SearchScope Subtree).DistinguishedName | ForEach-Object{
+    $fullList += ((Get-GPInheritance -Target $_).GpoLinks | Select-Object DisplayName, Target, Description, WMIFilter)
+    Write-Host "Links for" $_ " found..." #write to give status update
 }
 
-(Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase $StartOU -SearchScope Subtree).DistinguishedName | ForEach-Object{
-    $data = [PSCustomObject]@{ 
-        OU = $_.DistinguishedName
-    }
-    (Get-GPInheritance -Target $_.DistinguishedName).GpoLinks | ForEach-Object{
-        $data = [PSCustomObject]@{ 
-            DisplayName = (Get-GPO -Name ($_.DisplayName)).DisplayName
-            Description = (Get-GPO -Name ($_.DisplayName)).Description
-            WMIFilter   = (Get-GPO -Name ($_.DisplayName)).WMIFilter
-   
-        }
+Write-Host "List of links completed. Beginning additional GPO info gathering!" #write to give status update
 
+for($i=0; $i -lt $fullList.length; $i++){
+    if(((Get-GPO -Name (fullList[$i].DisplayName)).Description) -ne $null){
+        $fullList[$i].Description = (Get-GPO -Name ($fullList[$i].displayName)).Description
     }
+
+    if(((Get-GPO -Name (fullList[$i].DisplayName)).WMIFilter) -ne $null){
+        $fullList[$i].WMIFilter = (Get-GPO -Name ($fullList[$i].displayName)).WMIFilter
+    }
+
+    Write-Host "Description and WMI Filter grabbed for" $fullList[$i].Displayname #write to give status update
 }
+
+Write-Host "Writing to" $filePath #write to give status update
+Write-Output $fullList | Export-Csv $filePath
+Write-Host "Script Completed!" #write to give status update
